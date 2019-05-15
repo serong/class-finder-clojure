336179dbaab61093fae756de3a9f3d6c2e79f2cb
Serkan Ongan <serkan.ongan@gmail.com>
2019-05-15 19:35:11 +0300

    Update main.

--- src/cfinder/core.clj
+++ src/cfinder/core.clj
@@ -5,7 +5,7 @@
 (require '[clojure.string :as str])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Print the matching class names."
   [& args]
 
   (def lines
@@ -13,13 +13,10 @@
          (map str/trim)
          (filter #(not (= "" %)))
          (filter #(matches % (second args)))
-         (sort-by remove-package-name)
-         )
-    )
+         (sort-by remove-package-name)))
 
   (doseq
     [line lines]
-    (println ">>> " line)
-    )
+    (println line))
 
   )
